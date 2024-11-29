5992908b5d0afdeccaf1d20ea44f52dbff385ed4
Dipesh Patel <dipesh.patel@brainvire.com>
2024-11-29 17:54:02 +0530

    Initial Commit

---
+++ SwiftUIAnimations/SwiftUIAnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAnimationsApp.swift
+//  SwiftUIAnimations
+//
+//  Created by Dipesh Patel on 29/11/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
